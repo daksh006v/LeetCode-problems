cb3ac4026a7dafcb052c635a92e11aa5f30e1670
Daksh Bajaniya <daksh006v@gmail.com>
2026-06-04 11:53:33 +0530

    Time: 0 ms (100%), Space: 15.8 MB (96.06%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-          stack<int> st;
+          stack<int, vector<int>> st;
 
         for (string s : tokens) {
             if (s == "+" || s == "-" || s == "*" || s == "/") {
