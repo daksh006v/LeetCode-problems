4954bde0f93945a0077c50d2caacbf3b5c6f710b
Daksh Bajaniya <daksh006v@gmail.com>
2026-09-08 11:53:40 +0530

    Time: 7 ms (65.52%), Space: 11.2 MB (85.62%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,14 +9,32 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        set <ListNode*> address;
+        // set <ListNode*> address;
+        // ListNode* i = head;
+        // while (i != NULL){
+        //     if(address.count(i)){
+        //         return i;
+        //     }
+        //     address.insert(i);
+        //     i = i->next;
+        // }
+        // return NULL;
+
+        // soln 2:
         ListNode* i = head;
-        while (i != NULL){
-            if(address.count(i)){
-                return i;
-            }
-            address.insert(i);
+        ListNode* j = head;
+
+        while( j != NULL && j->next != NULL ){
             i = i->next;
+            j = j->next->next;
+
+            if(i == j){
+               while(head != i){
+                head = head -> next;
+                i = i->next;
+               }
+               return head;
+            }
         }
         return NULL;
     }
